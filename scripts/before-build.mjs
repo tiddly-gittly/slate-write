41e0ee9d1a7bd57f6fa5b8fd1beaf438ab9b6866
Lin Onetwo <linonetwo012@gmail.com>
2022-03-13 00:37:25 +0800

    feat: make a demo

--- scripts/before-build.mjs
+++ scripts/before-build.mjs
@@ -1,4 +1,6 @@
 const repoDir = path.resolve(__dirname, '..');
-const distDir = path.join(repoDir, 'dist', 'plugins', 'linonetwo', 'slate-write');
-// cross platform cp -r ${repoDir}/src/ ${distDir}/
-fs.copy(path.join(repoDir, 'src'), distDir);
+const pluginDistDir = path.join(repoDir, 'dist', 'plugins', 'linonetwo', 'slate-write');
+// cross platform cp -r ${repoDir}/src/ ${pluginDistDir}/
+fs.copy(path.join(repoDir, 'src'), pluginDistDir);
+// copy demo
+fs.copy(path.join(repoDir, 'demo'), path.join(repoDir, 'dist'));
