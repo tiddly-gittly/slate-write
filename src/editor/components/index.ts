fb9fb01058783ef20f66c51e2c93d781e29e7b69
Lin Onetwo <linonetwo012@gmail.com>
2022-05-07 22:56:30 +0800

    refactor: move withStyledDraggables to config folder

--- src/editor/components/index.ts
+++ src/editor/components/index.ts
@@ -1,6 +1,5 @@
-import { createPlateUI } from '@udecode/plate';
 import { CONFIG } from '../config/config';
-import { withStyledDraggables } from './withStyledDraggables';
-import { withStyledPlaceHolders } from './withStyledPlaceHolders';
+import { withStyledDraggables } from '../config/withStyledDraggables';
+import { withStyledPlaceHolders } from '../config/withStyledPlaceHolders';
 
 export const components = withStyledDraggables(withStyledPlaceHolders(CONFIG.components));
