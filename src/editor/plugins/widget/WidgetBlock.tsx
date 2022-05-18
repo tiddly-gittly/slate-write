021134e80a7b4eb6319642643a8e8b61f78b9e8e
Lin Onetwo <linonetwo012@gmail.com>
2022-05-18 11:27:26 +0800

    fix: white-space setting from slate break the macro rendering

--- src/editor/plugins/widget/WidgetBlock.tsx
+++ src/editor/plugins/widget/WidgetBlock.tsx
@@ -4,23 +4,30 @@ import React, { useRef } from 'react';
 import { IParseTreeNode } from 'tiddlywiki';
 import { useWidget } from 'tw-react';
 import { StyledElementProps, getRootProps } from '@udecode/plate-styled-components';
+import styled from 'styled-components';
 
 export interface IWidgetBlockProps {
   element: TElement & { node: IParseTreeNode };
 }
 export type WidgetBlockElementProps = StyledElementProps<Value, TElement & { node: IParseTreeNode }, IWidgetBlockProps>;
 
+const TwWidgetContainerOuter = styled.div``;
+const TwWidgetContainerInner = styled.div`
+  user-select: none;
+  white-space: normal;
+`;
+
 export function WidgetBlock(props: WidgetBlockElementProps): JSX.Element {
-  const { attributes, nodeProps, element, children, editor } = props;
+  const { attributes, element, children, editor } = props;
   const widgetContainerReference = useRef<HTMLDivElement>(null);
   useWidget(element.node, widgetContainerReference);
   const rootProps = getRootProps(props);
   return (
-    <div data-role="tw-widget-container" {...attributes} {...rootProps}>
-      <div style={{ userSelect: 'none' }} contentEditable={false} {...nodeProps}>
+    <TwWidgetContainerOuter data-role="tw-widget-container" {...attributes} {...rootProps} as={rootProps.as}>
+      <TwWidgetContainerInner contentEditable={false}>
         <div ref={widgetContainerReference} />
-      </div>
+      </TwWidgetContainerInner>
       {children}
-    </div>
+    </TwWidgetContainerOuter>
   );
 }
