4007ee15fa5415dd21ea3a9a9b2bae711e8ec5af
linonetwo <linonetwo012@gmail.com>
2023-06-27 00:34:37 +0800

    refactor: initial state hook

--- src/slate-write/editor/editor.tsx
+++ src/slate-write/editor/editor.tsx
@@ -1,33 +1,32 @@
+import { createPlugins, Plate, PlateProvider } from '@udecode/plate-core';
 import { createNodeIdPlugin } from '@udecode/plate-node-id';
-import useDebouncedCallback from 'beautiful-react-hooks/useDebouncedCallback';
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { useMemo } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-
-import { createPlugins, Plate, PlateProvider, usePlateActions, usePlateEditorRef, useResetPlateEditor } from '@udecode/plate-core';
-import { TElement, TNode } from '@udecode/slate';
-import { ReactEditor } from 'slate-react';
 import { IDefaultWidgetProps, ParentWidgetContext } from 'tw-react';
-import { deserialize, serialize } from '../transform/serialize';
+
 import { components } from './components';
 import { MacrosCombobox, SnippetCombobox, WidgetCombobox, WikiLinkCombobox } from './components/combobox';
 import { BallonToolbar } from './components/Toolbars';
 import { CONFIG } from './config/config';
 import { GlobalStyle } from './config/globalStyle';
 import * as PLUGINS from './config/plugins';
+import { useInitialValueOnChange } from './hooks/useInitialValueOnChange';
 import { getIdFactory } from './plugins/id/getId';
 
 export interface IEditorAppProps {
   currentTiddler: string;
+  idCreator: () => string;
   /** only changed on initial render, and when text updated by event outside of slate */
   initialTiddlerText: string;
-  saver: {
-    /** ms about debounce how long between save */
-    interval?: number;
-    /** a lock to prevent update from tiddler to slate, when update of tiddler is trigger by slate. */
-    lock: () => void;
-    onSave: (value: string) => void;
-  };
+  saver: ISaver;
+}
+export interface ISaver {
+  /** ms about debounce how long between save */
+  interval?: number;
+  /** a lock to prevent update from tiddler to slate, when update of tiddler is trigger by slate. */
+  lock: () => void;
+  onSave: (value: string) => void;
 }
 const defaultPlugins = createPlugins([...PLUGINS.basicElements, ...PLUGINS.basicMarks, ...PLUGINS.utils, ...PLUGINS.twAdvancedElements], {
   // Plate components
@@ -35,17 +34,8 @@ const defaultPlugins = createPlugins([...PLUGINS.basicElements, ...PLUGINS.basic
 });
 
 export function Editor(props: IEditorAppProps & IDefaultWidgetProps): JSX.Element {
-  const editorID = props.currentTiddler;
-  const { value: updateEditorValue } = usePlateActions(editorID);
-  const resetEditor = useResetPlateEditor(editorID);
-  const idCreator = useMemo(() => {
-    return getIdFactory(props.currentTiddler);
-  }, [props.currentTiddler]);
-  const editorReference = usePlateEditorRef(editorID);
-  // Add the initial value when setting up our state.
-  const currentAstReference = useRef<TElement[]>(deserialize(props.initialTiddlerText, { idCreator }));
-  /** current text is only used for compare, we don't want it trigger rerender, so use ref to store it */
-  const currentTextReference = useRef<string>(props.initialTiddlerText);
+  const { idCreator, currentTiddler: editorID } = props;
+
   // TODO: get dom node to add IME listener to prevent update when IME open https://github.com/udecode/plate/issues/239#issuecomment-1098052241
 
   // id plugin is vital for drag&drop
@@ -60,65 +50,8 @@ export function Editor(props: IEditorAppProps & IDefaultWidgetProps): JSX.Elemen
   );
   const plugins = useMemo(() => createPlugins([...defaultPlugins, idPlugin]), [idPlugin]);
 
-  // update current value from props
-  useEffect(() => {
-    // there will be cases that triple return replaced with double return (trim),  cause here rerender, but I think it is ok, not so frequent
-    if (currentTextReference.current !== props.initialTiddlerText) {
-      const newValue = deserialize(props.initialTiddlerText, { idCreator });
-      currentAstReference.current = newValue;
-      // reset selection, so if you delete wikitext, selection won't goto some empty space and cause error
-      resetEditor();
-      updateEditorValue(newValue);
-    }
-  }, [props.initialTiddlerText, currentTextReference, updateEditorValue, resetEditor, idCreator]);
-  const saver = useCallback(
-    (newValue: TNode[]) => {
-      // DEBUG: console
-      console.log(`newValue`, newValue);
-      const newText = serialize(newValue);
-      props.saver.onSave(newText);
-      currentTextReference.current = newText;
-    },
-    [props.saver.onSave],
-  );
-  const debouncedSaver = useDebouncedCallback(saver, [saver], props.saver.interval);
-  const onChange = useCallback(
-    (newValue: TElement[]) => {
-      if (currentAstReference.current === newValue) return;
-      // DEBUG: console
-      console.log(`currentAstReference.current = newValue`, newValue);
-      currentAstReference.current = newValue;
-      // check isComposing and if user is just set_selection
-      if (editorReference === null || ReactEditor.isComposing(editorReference as ReactEditor)) {
-        return;
-      }
-      if (editorReference.operations.every((op) => op.type === 'set_selection')) {
-        return;
-      }
-      props.saver.lock();
-      debouncedSaver(newValue);
-    },
-    [debouncedSaver, editorReference, props.saver],
-  );
-  useEffect(() => {
-    // reset selection, so if you delete wikitext, selection won't goto some empty space and cause error
-    // resetEditor();
-    // this only work as didMount
-    return () => {
-      // emergency save on close or switch to edit or readonly
-      saver(currentAstReference.current);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (typeof document === 'undefined') {
-    return <div>Loading...</div>;
-  }
-  // DEBUG: console
-  console.log(`currentAstRef.current`, currentAstReference.current);
-
   return (
-    <Plate id={editorID} initialValue={currentAstReference.current} plugins={plugins} onChange={onChange} editableProps={{ ...CONFIG.editableProps }}>
+    <Plate id={editorID} plugins={plugins} editableProps={{ ...CONFIG.editableProps }}>
       <BallonToolbar />
       <SnippetCombobox id={editorID} pluginKey='/' />
       <WikiLinkCombobox id={editorID} pluginKey='[[' />
@@ -130,13 +63,26 @@ export function Editor(props: IEditorAppProps & IDefaultWidgetProps): JSX.Elemen
 }
 
 export function App(props: IEditorAppProps & IDefaultWidgetProps): JSX.Element {
+  const editorID = props.currentTiddler;
+  const idCreator = useMemo(() => {
+    return getIdFactory(props.currentTiddler);
+  }, [props.currentTiddler]);
+  const [currentAstReference, onChange] = useInitialValueOnChange({
+    editorID,
+    initialTiddlerText: props.initialTiddlerText,
+    saver: props.saver,
+    idCreator,
+  });
+  if (typeof document === 'undefined') {
+    return <div>Loading...</div>;
+  }
   return (
-    <PlateProvider id={props.currentTiddler}>
+    <PlateProvider id={editorID} initialValue={currentAstReference.current} onChange={onChange}>
       <ParentWidgetContext.Provider value={props.parentWidget}>
         <GlobalStyle />
         <DndProvider backend={HTML5Backend}>
           <div className='tw-slate-write-container'>
-            <Editor {...props} />
+            <Editor {...props} idCreator={idCreator} />
           </div>
         </DndProvider>
       </ParentWidgetContext.Provider>
---
+++ src/slate-write/editor/hooks/useInitialValueOnChange.ts
@@ -0,0 +1,79 @@
+import { usePlateActions, usePlateEditorRef, useResetPlateEditor } from '@udecode/plate-core';
+import { TElement, TNode } from '@udecode/slate';
+import useDebouncedCallback from 'beautiful-react-hooks/useDebouncedCallback';
+import { useCallback, useEffect, useRef } from 'react';
+import { ReactEditor } from 'slate-react';
+
+import { deserialize, serialize } from '../../transform/serialize';
+import type { ISaver } from '../editor';
+
+interface IUseInitialValueOnChangeContext {
+  editorID: string;
+  idCreator: () => string;
+  initialTiddlerText: string;
+  saver: ISaver;
+}
+export function useInitialValueOnChange(context: IUseInitialValueOnChangeContext) {
+  const { editorID, initialTiddlerText, saver: { onSave, interval, lock }, idCreator } = context;
+  const resetEditor = useResetPlateEditor(editorID);
+  const editorReference = usePlateEditorRef(editorID);
+  const { value: updateEditorValue } = usePlateActions(editorID);
+
+  // Add the initial value when setting up our state.
+  const currentAstReference = useRef<TElement[]>(deserialize(initialTiddlerText, { idCreator }));
+  /** current text is only used for compare, we don't want it trigger rerender, so use ref to store it */
+  const currentTextReference = useRef<string>(initialTiddlerText);
+
+  // update current value from props
+  useEffect(() => {
+    // there will be cases that triple return replaced with double return (trim),  cause here rerender, but I think it is ok, not so frequent
+    if (currentTextReference.current !== initialTiddlerText) {
+      const newValue = deserialize(initialTiddlerText, { idCreator });
+      currentAstReference.current = newValue;
+      // reset selection, so if you delete wikitext, selection won't goto some empty space and cause error
+      resetEditor();
+      updateEditorValue(newValue);
+    }
+  }, [initialTiddlerText, currentTextReference, updateEditorValue, resetEditor, idCreator]);
+  const saver = useCallback(
+    (newValue: TNode[]) => {
+      const newText = serialize(newValue);
+      onSave(newText);
+      currentTextReference.current = newText;
+    },
+    [onSave],
+  );
+  const debouncedSaver = useDebouncedCallback(saver, [saver], interval);
+
+  const onChange = useCallback(
+    (newValue: TElement[]) => {
+      // DEBUG: console newValue
+      console.log(`newValue`, newValue);
+      if (currentAstReference.current === newValue) return;
+      currentAstReference.current = newValue;
+      // check isComposing and if user is just set_selection
+      if (editorReference === null || ReactEditor.isComposing(editorReference as ReactEditor)) {
+        return;
+      }
+      if (editorReference.operations.every((op) => op.type === 'set_selection')) {
+        return;
+      }
+      lock();
+      debouncedSaver(newValue);
+    },
+    [debouncedSaver, editorReference, lock],
+  );
+
+  useEffect(() => {
+    // reset selection, so if you delete wikitext, selection won't goto some empty space and cause error
+    // resetEditor();
+    // this only work as didMount
+    return () => {
+      // emergency save on close or switch to edit or readonly
+      saver(currentAstReference.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return [currentAstReference, onChange] as const;
+}
