3e81d01129df5265761033cbcfc4d5645ea4f9fd
lin onetwo <linonetwo012@gmail.com>
2024-10-05 00:13:20 +0800

    refactor: remove advanced blocks like codeblock, macro, widget, reimplement due to plate API change

--- src/slate-write/editor/plugins/codeblock/options/getCodeLineType.ts
+++
@@ -1,5 +0,0 @@
-import { PlateEditor, getPluginType } from '@udecode/plate-core';
-import { Value } from '@udecode/slate';
-import { ELEMENT_CODE_LINE } from '../constants';
-
-export const getCodeLineType = <V extends Value>(editor: PlateEditor<V>): string => getPluginType(editor, ELEMENT_CODE_LINE);
